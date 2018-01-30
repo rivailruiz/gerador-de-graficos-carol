899cab912aa80e2284877408ec35c3406e7a9e24
rivailruiz <alexander.ruiz@agenciaecan.com.br>
2018-01-30 18:14:44 -0200

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+$(window).load(function(){
+    $('#gerar').on('click', function(e){
+        e.preventDefault();
+        var pub1 = $('#pub1').val();
+        var alc1 = $('#alc1').val();
+
+        var pub2 = $('#pub2').val();
+        var alc2 = $('#alc2').val();
+
+        $('.area-grafico').css('background', 'white');
+        var ctx = document.getElementById("myChart");
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["Item 1", "Item 2"],
+                datasets: [{
+                    label: 'Gráfico carol',
+                    data: [pub1, pub2],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+        
+    })
+
+    
+});
